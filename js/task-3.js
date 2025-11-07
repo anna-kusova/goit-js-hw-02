e902ecfdb87ed991b0d2f9a7ed550eaea5ac85fa
Anna Kusova <kusovanna@gmail.com>
2025-11-07 13:35:01 +0200

    solved task for module 2 JS functions

---
+++ js/task-3.js
@@ -0,0 +1,26 @@
+// function checkForSpam(message) {
+//     const lowerMessage = message.toLowerCase();
+
+//         if (lowerMessage.includes("spam") || lowerMessage.includes("sale")) {
+//             return true;
+//         } else {
+//             return false;
+//         }
+    
+// }
+
+function checkForSpam(message) {
+    const lowerMessage = message.toLowerCase();
+    const hasSpam = lowerMessage.includes("spam");
+    const hasSale = lowerMessage.includes("sale");
+
+    return hasSpam || hasSale;
+}
+
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
